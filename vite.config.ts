a3493698648a51ce215b8530959f015bbf40f91a
Aiz <dev@aiz.moe>
2023-07-29 00:38:27 +0200

    feat(plugin): add image presets

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import ViteYaml from '@modyfi/vite-plugin-yaml';
+import imagePresets, { widthPreset } from 'vite-plugin-image-presets'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,10 +11,20 @@ export default defineConfig({
   plugins: [
     vue(),
     ViteYaml(),
+    imagePresets({
+        default: widthPreset({
+            widths: [200, 300],
+            sizes: '(max-width: 980px) 200px, 300px',
+            formats: {
+               webp: {quality: 100 },
+            },
+        }),
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('src', import.meta.url)),
+      '@images': resolve(__dirname, 'src/images')
     }
   }
 })

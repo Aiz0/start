adcf17291c4d89a940bf76035a13ed0d81ab6984
Aiz <dev@aiz.moe>
2023-07-29 03:01:52 +0200

    fix(vite): fix basename for custom domain site

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,29 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import ViteYaml from '@modyfi/vite-plugin-yaml';
+import ViteYaml from '@modyfi/vite-plugin-yaml'
 import imagePresets, { widthPreset } from 'vite-plugin-image-presets'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/start/',
-  plugins: [
-    vue(),
-    ViteYaml(),
-    imagePresets({
-        default: widthPreset({
-            widths: [200, 300],
-            sizes: '(max-width: 980px) 200px, 300px',
-            formats: {
-               webp: {quality: 100 },
-            },
+    base: '/',
+    plugins: [
+        vue(),
+        ViteYaml(),
+        imagePresets({
+            default: widthPreset({
+                widths: [200, 300],
+                sizes: '(max-width: 980px) 200px, 300px',
+                formats: {
+                    webp: { quality: 100 },
+                },
+            }),
         }),
-    }),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-      '@images': resolve(__dirname, 'src/assets/images')
-    }
-  }
+    ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@images': resolve(__dirname, 'src/assets/images'),
+        },
+    },
 })
